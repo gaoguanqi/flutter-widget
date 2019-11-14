5cc4596a218bae9cf5a0b1edeafed4f47e5f0c12
gaoguanqi <307590625@qq.com>
2019-11-14 13:12:07 +0800

    SizedBox

--- lib/pages/rotatedbox_page.dart
+++ lib/pages/rotatedbox_page.dart
@@ -53,6 +53,20 @@ class RotatedBoxPage extends StatelessWidget {
                   Icons.android
               ),
             ),
+
+            SizedBox(
+              height: 20.0,
+            ),
+
+            Stack(
+              children: <Widget>[
+                Image.network('https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K///it///u=4121232426,1436481329&fm=202&src=ry_add_wise'),
+                RotatedBox(
+                  quarterTurns: 1,
+                  child: Icon(Icons.audiotrack),
+                ),
+              ],
+            ),
           ],
         ),
       ),
