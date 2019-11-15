fa714e665f67c4d479dad10bb23d244ffdec1a1d
gaoguanqi <307590625@qq.com>
2019-11-15 10:48:05 +0800

    DropdownButton

--- lib/pages/dropdownbutton_page.dart
+++ lib/pages/dropdownbutton_page.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget/app/global/toast_utils.dart';
 
-class DropdownButtonPage extends StatelessWidget {
-  var _value;
+
+class DropdownButtonPage extends StatefulWidget {
+  @override
+  _DropdownButtonPageState createState() => _DropdownButtonPageState();
+}
+
+class _DropdownButtonPageState extends State<DropdownButtonPage> {
+  int _value;
+  String _txt = '下拉选择你想要的数据';
+  List<String> itemTxt = [
+    'item1',
+    'item2',
+    'item3',
+    'item4',
+    'item5',
+  ];
+  List<DropdownMenuItem<int>> items = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    for(var i = 0; i <itemTxt.length;i++){
+      DropdownMenuItem<int> item = DropdownMenuItem<int>(
+        child: Text(itemTxt[i]),
+        value: i,
+      );
+      items.add(item);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,56 +41,22 @@ class DropdownButtonPage extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         child: DropdownButton(
-          items: _items(),
-          hint: Text('下拉选择你想要的数据'),//当没有默认值的时候可以设置的提示
+          items: items,
+          hint: Text(_txt),//当没有默认值的时候可以设置的提示
           value: _value,
           icon: Icon(Icons.zoom_out,color: Colors.deepOrange,),
           iconSize: 48.0,
           style: TextStyle(color: Colors.blue,fontSize: 20.0,fontWeight: FontWeight.bold),
           onChanged: (value){
-
-            ToastUtils.showToast('点击:$value');
+           setState(() {
+             _value = value;
+             _txt = itemTxt[value];
+             ToastUtils.showToast('点击:$_value');
+           });
           },
         ),
       ),
     );
   }
-
-  _items() {
-    return <DropdownMenuItem>[
-      DropdownMenuItem<String>(
-        child: Text(
-          'item1'
-        ),
-        value: '1',
-      ),
-      DropdownMenuItem<String>(
-        child: Text(
-            'item2'
-        ),
-        value: '2',
-      ),
-
-      DropdownMenuItem<String>(
-        child: Text(
-            'item3'
-        ),
-        value: '3',
-      ),
-
-      DropdownMenuItem<String>(
-        child: Text(
-            'item4'
-        ),
-        value: '4',
-      ),
-      DropdownMenuItem<String>(
-        child: Text(
-            'item5'
-        ),
-        value: '5',
-      ),
-    ];
-  }
-
 }
+
